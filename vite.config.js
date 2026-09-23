8ef00b9ca5e21b8e451545f060ba2bfd8bcc199d
Your Name <you@example.com>
2026-09-23 11:55:48 +0530

    changes in vite config preview

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-import path from 'path';
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
   },
   server: {
-    port: 3000
+    port: 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: true,
   },
   optimizeDeps: {
-    exclude: ['recoil']
-  }
-})
+    exclude: ["recoil"],
+  },
+});
